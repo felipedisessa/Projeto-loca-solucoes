11bcaad38580d28018765aef7833d3ab3a7f5622
Felipe Di Sessa <felipetmds@hotmail.com>
2024-06-21 14:07:57 -0300

    Finalizado aba de relatorios

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Você está logado") }}
+                    {{ __("Bem vindo, ".auth()->user()->name) }}
                 </div>
                 <div id="calendar"></div>
             </div>
--- resources/views/reserves/edit.blade.php
+++ resources/views/reserves/edit.blade.php
@@ -77,6 +77,21 @@
             </div>
 
             <div class="relative z-0 w-full mb-5 group">
+                <select name="status" id="status"
+                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer">
+                    <option >Confirmado</option>
+                    <option>Pendente</option>
+
+                </select>
+                <label for="status" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                    Status
+                </label>
+                @error('status')
+                <div class= "text-amber-50">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="relative z-0 w-full mb-5 group">
                 <input type="text" name="payment_type" id="payment_type" value="{{ $reserve->payment_type }}"
                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                        placeholder=" "/>
